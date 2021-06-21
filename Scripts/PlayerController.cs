2857440135e1d345c90474cd4e6403dc8868efa8
Ilya <arzaevilya@gmail.com>
2021-06-21 02:07:26 +0200

    xRange

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horzontalInput;
+    public float speed = 10f;
+    public float xRange = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horzontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horzontalInput * Time.deltaTime * speed);
+    }
+}
