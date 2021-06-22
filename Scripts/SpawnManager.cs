ea818406699ba6a3d8b76111c5a0a15f3759b1a1
Ilya <arzaevilya@gmail.com>
2021-06-22 20:06:29 +0200

    game over

--- Scripts/SpawnManager.cs
+++ Scripts/SpawnManager.cs
@@ -8,21 +8,25 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterwal = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterwal);   
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
+
+    }
+
+    void SpawnRandomAnimal()
+    {
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
     }
 }
