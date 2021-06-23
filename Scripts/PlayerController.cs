2bdc2a8e3d543da25208e1aea36764c18030c6d7
Ilya <arzaevilya@gmail.com>
2021-06-23 05:18:33 +0200

    vertical gameplay

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,8 +6,10 @@ public class PlayerController : MonoBehaviour
 {
 
     public float horzontalInput;
+    public float verticalInput;
     public float speed = 10f;
     public float xRange = 10f;
+    public float zRange = -1f;
 
     public GameObject projectilePrefab;
 
@@ -28,13 +30,24 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z < zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+        if (transform.position.z > 16f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 16f);
+        }
 
         horzontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * horzontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
+
     }
 }
